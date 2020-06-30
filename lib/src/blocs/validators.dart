a5571ca584f9888cad5537c034f9c0f28539cafe
Andrew Randan <andrewrandan@gmail.com>
2020-06-30 17:00:32 +0300

    added bloc files

---
+++ lib/src/blocs/validators.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'package:email_validator/email_validator.dart';
+
+class Validators {
+  final validateEmail = StreamTransformer<String, String>.fromHandlers(
+    handleData: (email, sink) =>
+      EmailValidator.validate(email)
+        ? sink.add(email)
+        : sink.addError('Please enter a valid email')
+  );
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+    handleData: (password, sink) =>
+      password.length >= 4
+        ? sink.add(password)
+        : sink.addError('Password must be at least 4 characters')
+  );
+}
--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../blocs/bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   Widget build(context) =>
@@ -6,10 +7,36 @@ class LoginScreen extends StatelessWidget {
       margin: EdgeInsets.symmetric(vertical: 25.0, horizontal: 10.0),
       child: Column(
         children: [
-          // emailField(),
-          // passwordField(),
-          // submitButton()
+          emailField(),
+          passwordField(),
+          submitButton()
         ]
       )
+    );
+
+  Widget emailField() =>
+    TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'you@example.com'
+      )
+    );
+
+  Widget passwordField() =>
+    TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password'
+      )
+    );
+
+  Widget submitButton() =>
+    RaisedButton(
+      child: Text('Login'),
+      color: Colors.orange,
+      onPressed: () {
+        return null;
+      }
     );
 }

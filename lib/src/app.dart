6cf1b08b224b17512304a4a133942a3662b0e7b0
Andrew Randan <andrewrandan@gmail.com>
2020-07-03 15:11:42 +0300

    added Provider to app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import './screens/login_screen.dart';
+import 'blocs/provider.dart';
 
 class App extends StatelessWidget{
   Widget build(context) =>
-    MaterialApp(
-      title: 'Log Me In',
-      home: Scaffold(
-        body: LoginScreen()
+    Provider(
+      child: MaterialApp(
+        title: 'Log Me In',
+        home: Scaffold(
+          body: LoginScreen()
+        )
       )
     );
 }
